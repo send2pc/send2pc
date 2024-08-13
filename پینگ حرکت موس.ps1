49c6f0e6df0e6509afc3caefeb36706606be2772
send2pc <153140346+send2pc@users.noreply.github.com>
2024-08-13 12:21:15 +0330

    move ping

---
+++ پینگ حرکت موس.ps1
@@ -0,0 +1,54 @@
+﻿Add-Type -TypeDefinition @"
+using System;
+using System.Runtime.InteropServices;
+
+public class MouseHelper {
+    [DllImport("user32.dll")]
+    public static extern bool SetCursorPos(int X, int Y);
+
+    [DllImport("user32.dll")]
+    public static extern bool GetCursorPos(out POINT lpPoint);
+
+    public struct POINT {
+        public int X;
+        public int Y;
+    }
+
+    public static void MoveCursorSlightly() {
+        POINT point;
+        GetCursorPos(out point);
+        SetCursorPos(point.X + 1, point.Y);  // حرکت مکان‌نما به اندازه 1 پیکسل به سمت راست
+    }
+}
+"@
+
+function Test-Ping {
+    param (
+        [string[]]$Addresses
+    )
+    
+    $random = New-Object System.Random
+    $address = $Addresses[$random.Next($Addresses.Length)]
+    
+    try {
+        $pingResult = Test-Connection -ComputerName $address -Count 1 -Quiet
+        return $pingResult
+    }
+    catch {
+        Write-Output "An error occurred during ping: $_"
+        return $false
+    }
+}
+
+# لیست سرورهای پینگ
+$pingAddresses = @("1.1.1.1", "8.8.8.8", "9.9.9.9")  # اضافه کردن آدرس‌های دلخواه
+
+while ($true) {
+    if (Test-Ping -Addresses $pingAddresses) {
+        [MouseHelper]::MoveCursorSlightly()
+    }
+    
+    # زمان انتظار برای شبیه‌سازی پینگ (می‌توانید زمان‌بندی را تغییر دهید)
+        Start-Sleep -Milliseconds 500
+
+}
